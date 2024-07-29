6add8dedf71b4dd1389a8f0810f1722eeff7081e
Chris Moore <chrismoorelmt@gmail.com>
2024-07-29 00:21:53 -0500

    for loops for fetch data, restore default function, troubleshooting, no fix yet

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,57 @@
-const city = "cityInput";
 const apiKey = "4487b9303d1ed3329bcd012966489cc8";
-const queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${newCity.value}&appid=${apiKey}`;
+const newCity = document.getElementById("cityInput");
+const d = new Date();
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 function getInfo() {
-    const newCity = document.getElementById("cityInput");
+    const queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${newCity.value}&appid=${apiKey}&units=imperial`;
     const localHeader = document.getElementById("localHeader");
-    localHeader.innerHTML = newCity.value;
-}
 
-fetch (queryURL)
+    localHeader.innerHTML = newCity.value;
+    
+    fetch (queryURL)
     .then(response => {
         if (!response.ok) {
             throw new Error('Network response was not ok.');
         }
         return response.json();
     })
-
+    
     .then(data => {
-        for(i = 0; i < 5; i++){
-            document.getElementById("day" + (i + 1) + "temp").innerHTML ="temp"
+        for(let i = 0; i < 5; i++){
+            document.getElementById("img" + (i + 1)).src ="https://openweathermap.org/img/wn/" +data.list[i].weather0.icon+".png";
+        }
+        for(let i = 0; i < 5; i++){
+            document.getElementById("day" + (i + 1) + "Temp").innerHTML ="Temp:" +Number(data.list[i].main.temp).toFixed(1)+"°";
+        }
+        for(let i = 0; i < 5; i++){
+            document.getElementById("day" + (i + 1) + "Wind").innerHTML ="Wind:" +Number(data.list[i].wind.speed).toFixed(0)+"mph";
+        }
+        for(let i = 0; i < 5; i++){
+            document.getElementById("day" + (i + 1) + "Humidity").innerHTML ="Humidity:" +Number(data.list[i].main.humidity).toFixed(0)+"%";
         }
     })
     .catch(error => {
         console.error('There was a problem with fetch:', error);
     });
+    
+};
 
-//api.openweathermap.org/data/2.5/forecast?q=houston&appid=4487b9303d1ed3329bcd012966489cc8;
-//API Key: 4487b9303d1ed3329bcd012966489cc8
+function restoreDefault() {
+    document.getElementById("cityInput").defaultValue = "Houston";
+    getInfo();
+}
+
+//! Correct syntax of line 22. Figure out how to correctly fetch day of week. Continue to struggle with appending elements.
+
+/*function checkDay(day) {
+    if (day +d.getDay() > 6) {
+        return day +d.getDay() - 7;
+    } else {
+        return day +d.getDay();
+    }
+};
+
+for(i = 0; i < 5; i++) {
+    document.getElementById("day" + (i + 1)).innerHTML = weekday[checkDay(i)];
+};*/
