7724afe3dfaa7504ad096a3bd2f5241d933eed66
Chris Moore <chrismoorelmt@gmail.com>
2024-08-10 14:29:35 -0500

    Added some functionality, ready to deploy but some functionality is missing, bug fix needed

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,6 @@
 const apiKey = "4487b9303d1ed3329bcd012966489cc8";
 const newCity = document.getElementById("cityInput");
-const d = new Date();
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
 
 function getInfo() {
     const queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${newCity.value}&appid=${apiKey}&units=imperial`;
@@ -9,6 +8,13 @@ function getInfo() {
 
     localHeader.innerHTML = newCity.value;
     
+    function currentWeather(data) {
+        document.getElementById("headerImg".src ="https://openweathermap.org/img/wn/" +data.list[0].weather[0].icon+".png");
+        document.getElementById("headerTemp").innerHTML ="Temp: " +Number(data.list[0].main.temp).toFixed(1)+"°";
+        document.getElementById("headerWind").innerHTML ="Wind: " +Number(data.list[0].wind.speed).toFixed(0)+"mph";
+        document.getElementById("headerHumidity").innerHTML ="Humidity: " +Number(data.list[0].main.humidity).toFixed(0)+"%";
+    };
+
     fetch (queryURL)
     .then(response => {
         if (!response.ok) {
@@ -19,39 +25,43 @@ function getInfo() {
     
     .then(data => {
         for(let i = 0; i < 5; i++){
-            document.getElementById("img" + (i + 1)).src ="https://openweathermap.org/img/wn/" +data.list[i].weather0.icon+".png";
+            document.getElementById("img" + (i + 1)).src ="https://openweathermap.org/img/wn/" +data.list[i].weather[0].icon+".png";
         }
         for(let i = 0; i < 5; i++){
-            document.getElementById("day" + (i + 1) + "Temp").innerHTML ="Temp:" +Number(data.list[i].main.temp).toFixed(1)+"°";
+            document.getElementById("day" + (i + 1) + "Temp").innerHTML ="Temp: " +Number(data.list[i].main.temp).toFixed(1)+"°";
         }
         for(let i = 0; i < 5; i++){
-            document.getElementById("day" + (i + 1) + "Wind").innerHTML ="Wind:" +Number(data.list[i].wind.speed).toFixed(0)+"mph";
+            document.getElementById("day" + (i + 1) + "Wind").innerHTML ="Wind: " +Number(data.list[i].wind.speed).toFixed(0)+"mph";
         }
         for(let i = 0; i < 5; i++){
-            document.getElementById("day" + (i + 1) + "Humidity").innerHTML ="Humidity:" +Number(data.list[i].main.humidity).toFixed(0)+"%";
+            document.getElementById("day" + (i + 1) + "Humidity").innerHTML ="Humidity: " +Number(data.list[i].main.humidity).toFixed(0)+"%";
         }
+        currentWeather(data);
     })
     .catch(error => {
         console.error('There was a problem with fetch:', error);
     });
-    
+};
+
+function storeSearch(newCity) {
+    const cityHistory = JSON.parse(localStorage.getItem('cities')) || [];
+    if (cityHistory.includes(newCity)) {
+        return;
+    } else {
+        cityHistory.push(newCity);
+        localStorage.setItem('cities', JSON.stringify(cityHistory));
+        displayCities();
+    }
 };
 
 function restoreDefault() {
     document.getElementById("cityInput").defaultValue = "Houston";
     getInfo();
-}
-
-//! Correct syntax of line 22. Figure out how to correctly fetch day of week. Continue to struggle with appending elements.
-
-/*function checkDay(day) {
-    if (day +d.getDay() > 6) {
-        return day +d.getDay() - 7;
-    } else {
-        return day +d.getDay();
-    }
 };
 
-for(i = 0; i < 5; i++) {
-    document.getElementById("day" + (i + 1)).innerHTML = weekday[checkDay(i)];
-};*/
+//ToDo: Make sure search history is saved to local storage.
+//ToDo: Add displayCities function to display search history in sidebar.
+//ToDo: Items in search history must be clickable and return new search on click.
+//ToDo: You forgot to add dates to the API results in forecast cards.
+//ToDo: Consolidate for loops, lines 27-38.
+//!Bug fix: Weather icon does not appear on current weather card.
