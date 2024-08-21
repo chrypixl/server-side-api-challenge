d8e3707c3271549b9ee3dfbd355ff4ca5ed50071
Chris Moore <chrismoorelmt@gmail.com>
2024-08-20 23:46:41 -0500

    Bug fix for deployed page, corrected error in which API request wouldn't work at all

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,20 @@
 const apiKey = "4487b9303d1ed3329bcd012966489cc8";
-const newCity = document.getElementById("cityInput");
+const newCity = document.getElementById('cityInput');
+const cityHistory = JSON.parse(localStorage.getItem('cities')) || [];
 
+
+function storeSearch(newCity) {
+    if (cityHistory.includes(newCity)) {
+        return;
+    } else {
+        cityHistory.push(newCity);
+        localStorage.setItem('cities', JSON.stringify(cityHistory));
+        displayCities();
+    }
+};
 
 function getInfo() {
-    const queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${newCity.value}&appid=${apiKey}&units=imperial`;
+    const queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${newCity.value}&appid=${apiKey}&units=imperial`;
     const localHeader = document.getElementById("localHeader");
 
     localHeader.innerHTML = newCity.value;
@@ -13,6 +24,7 @@ function getInfo() {
         document.getElementById("headerTemp").innerHTML ="Temp: " +Number(data.list[0].main.temp).toFixed(1)+"°";
         document.getElementById("headerWind").innerHTML ="Wind: " +Number(data.list[0].wind.speed).toFixed(0)+"mph";
         document.getElementById("headerHumidity").innerHTML ="Humidity: " +Number(data.list[0].main.humidity).toFixed(0)+"%";
+        document.getElementById('headerDate').textContent=dayjs.unix(data.dt).format('MM-DD-YYYY');
     };
 
     fetch (queryURL)
@@ -26,14 +38,8 @@ function getInfo() {
     .then(data => {
         for(let i = 0; i < 5; i++){
             document.getElementById("img" + (i + 1)).src ="https://openweathermap.org/img/wn/" +data.list[i].weather[0].icon+".png";
-        }
-        for(let i = 0; i < 5; i++){
             document.getElementById("day" + (i + 1) + "Temp").innerHTML ="Temp: " +Number(data.list[i].main.temp).toFixed(1)+"°";
-        }
-        for(let i = 0; i < 5; i++){
             document.getElementById("day" + (i + 1) + "Wind").innerHTML ="Wind: " +Number(data.list[i].wind.speed).toFixed(0)+"mph";
-        }
-        for(let i = 0; i < 5; i++){
             document.getElementById("day" + (i + 1) + "Humidity").innerHTML ="Humidity: " +Number(data.list[i].main.humidity).toFixed(0)+"%";
         }
         currentWeather(data);
@@ -43,26 +49,14 @@ function getInfo() {
     });
 };
 
-function storeSearch(newCity) {
-    const cityHistory = JSON.parse(localStorage.getItem('cities')) || [];
-    if (cityHistory.includes(newCity)) {
-        return;
-    } else {
-        cityHistory.push(newCity);
-        localStorage.setItem('cities', JSON.stringify(cityHistory));
-        displayCities();
-    }
-};
 
-function restoreDefault() {
-    document.getElementById("cityInput").defaultValue = "Houston";
-    getInfo();
-};
+// function restoreDefault() {
+//     document.getElementById("cityInput").defaultValue = "Houston";
+//     getInfo();
+// };
 
 //ToDo: Make sure search history is saved to local storage.
 //ToDo: Add displayCities function to display search history in sidebar.
 //ToDo: Items in search history must be clickable and return new search on click.
 //ToDo: You forgot to add dates to the API results in forecast cards.
-//ToDo: Consolidate for loops, lines 27-38.
 //!Bug fix: Weather icon does not appear on current weather card.
-//!Bug fix: New issue discovered - Application will not work if using the deployed link, but works fine when opening index.html in the browser. 
